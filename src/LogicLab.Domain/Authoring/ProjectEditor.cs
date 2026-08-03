7abedc5dedbe200c66cb95a2533917bc8cfca361
Romeo Ahmed <ahmedorqwn@gmail.com>
2026-08-03 12:38:45 +0800

    fix: guard interactive editor commands
    Signed-off-by: Romeo Ahmed <ahmedorqwn@gmail.com>

--- src/LogicLab.Domain/Authoring/ProjectEditor.cs
+++ src/LogicLab.Domain/Authoring/ProjectEditor.cs
@@ -110,6 +110,11 @@ public static partial class ProjectEditor
             return Reject(MissingReference("circuitDefinition"));
         }
 
+        if (revision.Document.EntryCircuitDefinitionId == intent.CircuitDefinitionId)
+        {
+            return new EditCommitted(revision, [], []);
+        }
+
         var document = revision.Document.WithEntryCircuitDefinition(
             intent.CircuitDefinitionId);
         return Commit(
